87128996e4788fc14b4e86a8d16c3666d8b97f87
TaritNatuntong <tarit.natuntong@apar.com>
2026-01-04 17:41:02 +0700

    asdasd

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,6 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test('Math check', () {
     // แกล้งบอกว่า 1 + 1 ต้องได้ 5 (ผิด Logic!)
-    expect(1 + 1, 5);
+    expect(1 + 1, 6);
   });
 }
