fd065b669f275ae2e0dd6a094aca30dfd352f986
TaritNatuntong <tarit.natuntong@apar.com>
2026-01-05 09:47:26 +0700

    feat: unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,6 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test('Math check', () {
     // 1 + 1 ต้องเท่ากับ 2 (อันนี้ผ่านแน่ๆ)
-    expect(1 + 1, 7);
+    expect(1 + 1, 2);
   });
 }
